394b5b7f8b983b7270ea08404ce163282d9945b1
Clint Wylie <cwylie@apache.org>
2019-08-26 18:26:36 -0700

    add scripts to build release documentation

---
+++ release.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -eu
+
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+if [ "$#" -ne 1 ]; then
+  echo "Illegal number of parameters, should be just one: the version";
+  exit 1;
+fi
+echo "Building to docs for '$1'..."
+
+baseDir=$(pwd)
+
+pushd $baseDir
+rm -rf _staging/
+mkdir -p _staging/
+cd _staging/
+git clone git@github.com:apache/incubator-druid.git
+cd incubator-druid/
+git checkout $1
+mvn -Pwebsite-docs -pl website compile -Dwebsite.src=$baseDir
+popd
+
+git add .
+
+echo "Building Website..."
+npm run build
+pushd $(pwd)
+cd ../incubator-druid-website/
+git add .
+popd
