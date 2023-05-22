69392e4c6993bd882125e7a263ce1c7bf0eb06ae
brian.le <brian.le@imply.io>
2023-05-22 11:48:28 -0700

    add apache license

--- static/js/FeatureList.js
+++ static/js/FeatureList.js
@@ -2,6 +2,26 @@
 Used by features.js in index.js to display the cards under the heading Key Druid Features. You can just add another more, and it'll keep adding cards in a 3 column pattern defined in index.css
 */
 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ /* Add copy-to-clipboard buttons to code blocks */
+
 export const FeatureList = [
   {
     "title": "Interactive Query Engine",
--- static/js/version.js
+++ static/js/version.js
@@ -10,6 +10,26 @@ versionPrevious is the currently live release and its corresponding release date
 urlPath is the value that's used for routing. The latest Druid docs are served on `latest` and its version, such as 25.0.0. So you'll need to build once with urlPath set to `latest` and once with it set to the version number.
 */
 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ /* Add copy-to-clipboard buttons to code blocks */
+
 module.exports = {
   version: "25.0.0",
   date: "Jan 4 2023",
