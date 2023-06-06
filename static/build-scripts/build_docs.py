49a6e42b11f4747ccde7f238e999f286e536d870
Victoria Lim <lim.t.victoria@gmail.com>
2023-06-06 16:44:48 -0700

    update scripts and add a main script

--- static/build-scripts/build_docs.py
+++ static/build-scripts/build_docs.py
@@ -3,17 +3,14 @@
 """
 build-docs.py
 
-Version:        5 June 2023
+Version:        6 June 2023
 
 Purpose:        Build the OSS Druid Docusaurus 2 docs for all
                 versions supplied in the [-v, --versions] flag.
                 Versions aside from "latest" only retain content
                 from the docs folder. The "latest" version keeps
-                everything.
-
-Assumes:        - Script called from the TBD directory
-                - You have already installed the site using
-                  "yarn install" or "npm run install"
+                everything. Assumes script is called from
+                `druid-website-src/static/build-scripts` directory.
 
 Help:           python build-docs.py --help
 
@@ -62,10 +59,31 @@ def build_docs(versions):
             for line in fileinput.input("redirects.js", inplace=1):
                 print(line.replace(f"/{v}/", "/latest/"), end='')
 
-#    # after all version builds, rename the temp directory back to "build"
-#    shutil.move(destination_dir, source_dir)
+    # after all version builds, rename the temp directory back to "build"
+    shutil.rmtree(source_dir)
+    shutil.move(destination_dir, source_dir)
 
-def main():
+def main(versions, skip_install):
+
+    # from druid-website-src/static/build-scripts,
+    # move to druid-website-src to run the npm commands
+    os.chdir("../../")
+
+    # sort to build "latest" last
+    versions = sorted(versions)
+
+    # install docusaurus 2
+    if not skip_install:
+        print("Installing Docusaurus 2...")
+        subprocess.run(["npm", "install"])
+
+    # remove the old build directory
+    shutil.rmtree('build', ignore_errors=True)
+
+    # do the actual builds
+    build_docs(versions)
+
+if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
 
@@ -73,17 +91,11 @@ def main():
                         help="One or more versions to build. "
                         "For example: -v latest 26.0.0")
 
+    parser.add_argument("--skip-install",
+                        help="Skip the Docusaurus 2 installation",
+                        action='store_true')
+
     args = parser.parse_args()
 
-    # sort to build "latest" last
-    versions = sorted(args.versions)
-
-#    # install docusaurus 2
-#    print("Installing Docusaurus 2...")
-#    subprocess.run(["npm", "install"])
-
-    build_docs(versions)
-
-if __name__ == "__main__":
-    main()
+    main(args.versions, args.skip_install)
 
---
+++ static/build-scripts/copy_druid_docs.py
@@ -0,0 +1,91 @@
+import shutil
+from distutils.dir_util import copy_tree
+import os
+
+"""
+# Copies over the docs folder from `apache/druid` to `druid-website-src into the version for it, e.g. druid-website-src/docs/26.0.0`
+# Prompts the user on whether or not this is the highest available version for download. If yes, also copies the docs to `druid-website-src/docs/latest`
+# Copies over sidebars and redirects
+# Replaces {{DRUIDVERSION}} in the markdown files with the druid version you specify for the variable
+"""
+
+druid_variable = "{{DRUIDVERSION}}"
+
+# Make sure you have the correct branch checked out for `apache/druid`
+# The directory structure should have apache/druid and apache/druid-website-src as peers
+# Set source_directory to your OSS Druid repo
+source_directory = "../../../druid/"
+
+
+# Find/replace {{DRUIDVERSION}} with the actual version
+def replace_text_in_file(destination_directory, druid_version):
+    with open(destination_directory, 'r') as file:
+        file_content = file.read()
+
+    modified_content = file_content.replace(druid_variable, druid_version)
+
+    with open(destination_directory, 'w') as file:
+        file.write(modified_content)
+
+def do_the_replace(file_path, druid_version):
+    for item in os.listdir(file_path):
+        item_path = os.path.join(file_path, item)
+
+        # If the item is a file and has a markdown extension
+        if os.path.isfile(item_path) and item_path.lower().endswith(".md"):
+            replace_text_in_file(item_path, druid_version)
+
+        # If the item is a file and has a markdownx extension
+        if os.path.isfile(item_path) and item_path.lower().endswith(".mdx"):
+            replace_text_in_file(item_path, druid_version)
+
+        # If the item is a directory, recursively process its contents
+        elif os.path.isdir(item_path):
+            do_the_replace(item_path, druid_version)
+
+def is_it_latest(druid_version, destination_directory_latest):
+
+  is_latest = input(f"Is {druid_version} going to be the highest version available for download? If yes, the docs will also be used for 'latest'. (y/n) ").lower()
+
+  if is_latest == 'y':
+      print("Also copying the docs to docs/latest.")
+      copy_tree(source_directory+'docs', destination_directory_latest)
+      shutil.rmtree(f"{destination_directory_latest}/_bin")
+      do_the_replace(destination_directory_latest, druid_version)
+  elif is_latest == 'n':
+      print("Not copying the docs to docs/latest")
+  else:
+      print("Enter y or n to make a choice")
+
+
+def main(druid_version):
+
+
+    # The destination_directory should be druid-website-src/docs/VERSION
+    destination_directory = f"../../docs/{druid_version}"
+    destination_directory_latest = "../../docs/latest"
+
+    # Copies the docs
+    copy_tree(source_directory+"docs",destination_directory)
+
+    # deletes the _bin directory that's not needed
+    shutil.rmtree(f"{destination_directory}/_bin")
+
+    # Copy sidebars.json and redirects.json
+    shutil.copyfile(source_directory+"website/sidebars.json","../../sidebars.json")
+    shutil.copyfile(source_directory+"website/redirects.json","../../redirects.js")
+
+    do_the_replace(destination_directory, druid_version)
+
+    is_it_latest(druid_version, destination_directory_latest)
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-v", "--version", required=True,
+                        help="Version to copy. Do not include 'latest'. "
+                        "For example: -v 26.0.0")
+    args = parser.parse_args()
+
+    main(args.version)
